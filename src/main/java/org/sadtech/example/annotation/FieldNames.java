06adcbab9dcc83d470f311b939ce01a03732648d
uPagge <git@upagge.ru>
2021-06-20 14:14:27 +0300

    final version

---
+++ src/main/java/org/sadtech/example/annotation/FieldNames.java
@@ -0,0 +1,14 @@
+package org.sadtech.example.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.CLASS)
+public @interface FieldNames {
+
+    String postfix() default "Fields";
+
+}
